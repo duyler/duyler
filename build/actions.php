a192841fbbc2ad48dea185fc2b0d73c05b67bac6
Mikhail Ilinsky <mail@milinsky.ru>
2023-08-26 18:19:54 +1000

    Add demo actions

--- build/actions.php
+++ build/actions.php
@@ -3,3 +3,18 @@
 declare(strict_types=1);
 
 use Duyler\Framework\Facade\Action;
+
+Action::add(
+    id: 'Duyler.SayHello',
+    handler: function() {
+        echo ' - Hello, World! <br>';
+    },
+    externalAccess: true,
+);
+
+Action::add(
+    id: 'World.SayHello',
+    handler: function() {
+        echo ' - Hi, Duyler! <br>';
+    },
+);
--- build/subscriptions.php
+++ build/subscriptions.php
@@ -4,3 +4,9 @@ declare(strict_types=1);
 
 use Duyler\EventBus\Enum\ResultStatus;
 use Duyler\Framework\Facade\Subscription;
+
+Subscription::add(
+    subjectId: 'Duyler.SayHello',
+    actionId: 'World.SayHello',
+    status: ResultStatus::Success
+);
